40954d9e3bda1aaa9c19cd548c2030a90288dff8
桂涛 <49463498+batmanTT3@users.noreply.github.com>
2021-09-14 16:10:30 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import { router } from "./router/index"; // router
+
+import store from "./store"; // store
+
+import directive from "./directive/index"; // directive
+
+import "element-plus/lib/theme-chalk/index.css"; // element css
+
+import useElementUi from "./elementui"; // 按需引入element-ui
+
+import "./core/permission"; // 动态校验路由
+
+import "./stylus/index.styl"; // css文件
+
+import svgIcon from "./plugin/icons/svgIcon.vue"; // svg文件
+
+const app = createApp(App);
+
+directive(app); // 放在mount上面，有顺序的
+
+useElementUi(app); // 引用element-ui
+
+app.component("svg-icon", svgIcon);
+
+app.use(store);
+
+app.use(router);
+
+app.mount("#app");
---
+++ vite.config.js
@@ -0,0 +1,50 @@
+const ip = require("ip");
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import styleImport from "vite-plugin-style-import";
+import { svgBuilder } from "./src/plugin/icons/svgBuilder";
+
+const path = require("path");
+
+function _resolve(dir) {
+  return path.resolve(__dirname, dir);
+}
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": _resolve("src"),
+    },
+  },
+  plugins: [
+    vue(),
+    svgBuilder("./src/plugin/icons/svg/"),
+    /* styleImport({
+      libs: [
+        {
+          libraryName: "element-plus",
+          esModule: true,
+          resolveStyle: (name) => {
+            return `element-plus/lib/theme-chalk/${name}.css`;
+          },
+          resolveComponent: (name) => {
+            return `element-plus/lib/${name}`;
+          },
+        },
+      ],
+    }), */
+  ],
+  server: {
+    port: 9000,
+    cors: true,
+    host: ip.address(),
+    /* proxy: {
+      "/epgms": {
+        target: "http://10.10.8.14:9999/mock/5f23d33facd7311a719430ed/epgms/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/epgms/, ""),
+      },
+    }, */
+  },
+});
